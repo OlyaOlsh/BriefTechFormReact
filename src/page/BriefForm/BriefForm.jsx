d302aaca99f5a57201e7d083db905b75c21a2057
olgaolsh <oliprogram@gmail.com>
2024-09-26 23:25:38 +0300

    10change

--- src/page/BriefForm/BriefForm.jsx
+++ src/page/BriefForm/BriefForm.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './BriefForm.css';
 
-const inputClasses = 'w-full px-4 py-3 border border-muted rounded-md focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent transition duration-200';
-const containerClasses = 'bg-background text-primary-foreground p-8 rounded-lg shadow-lg max-w-lg mx-auto min-h-screen flex flex-col justify-between';
-const labelClasses = 'block text-sm font-medium mb-1 text-secondary';
+const inputClasses = 'w-full px-4 py-3 border border-muted rounded-md focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent transition duration-200 font-MarvelSans-Regular';
+const containerClasses = 'bg-background text-primary-foreground p-8 rounded-lg shadow-lg max-w-lg mx-auto min-h-screen flex flex-col justify-between font-MarvelSans-Regular';
+const labelClasses = 'block text-sm font-medium mb-1 text-secondary font-MarvelSans-Regular label-custom';
 
 const BriefForm = () => {
     const tg = window.Telegram.WebApp;
@@ -15,13 +15,40 @@ const BriefForm = () => {
     });
     
     const [errors, setErrors] = useState({});
+
+
+    const textareaAudienceRef = useRef(null); // Реф для textarea целевой аудитории
+    const textareaGoalsRef = useRef(null); // Реф для textarea целей
+
     
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormData((prevData) => ({ ...prevData, [name]: value }));
         setErrors((prevErrors) => ({ ...prevErrors, [name]: '' })); // Сбрасываем ошибку при изменении
+
+        // Изменяем высоту textarea
+        if (event.target.name === 'goals' && textareaGoalsRef.current) {
+            textareaGoalsRef.current.style.height = 'auto'; // Сбрасываем высоту
+            textareaGoalsRef.current.style.height = `${textareaGoalsRef.current.scrollHeight}px`; // Устанавливаем новую высоту
+        } else if (event.target.name === 'audience' && textareaAudienceRef.current) {
+            textareaAudienceRef.current.style.height = 'auto'; // Сбрасываем высоту
+            textareaAudienceRef.current.style.height = `${textareaAudienceRef.current.scrollHeight}px`; // Устанавливаем новую высоту
+        }
     };
 
+
+      // Устанавливаем начальную высоту при монтировании компонента
+      useEffect(() => {
+        if (textareaGoalsRef.current) {
+            textareaGoalsRef.current.style.height = '160px'; // Установите желаемую начальную высоту для целей
+        }
+        if (textareaAudienceRef.current) {
+            textareaAudienceRef.current.style.height = '160px'; // Установите желаемую начальную высоту для целевой аудитории
+        }
+    }, []);
+  
+
+    
     const validateForm = () => {
         const newErrors = {};
         if (!formData.projectName) newErrors.projectName = "Название проекта обязательно.";
@@ -58,14 +85,14 @@ const BriefForm = () => {
         <form onSubmit={handleSubmit} className="relative flex-grow">
             <div className={containerClasses}>
                 <span className={'username'}>{tg.initDataUnsafe?.user?.userName}</span>
-                <h2 className="text-3xl font-bold text-center mb-6 text-accent">Идея в MS Dynamics AX</h2>
+                <h2 className="custom-header text-3xl font-bold">Идея в MS Dynamics AX</h2>
 
                 <div className="mb-6">
                     <label htmlFor="project-name" className={labelClasses}>
                         Название проекта
                     </label>
                     <input type="text" id="project-name" name="projectName" placeholder="Укажите краткое название" className={inputClasses} value={formData.projectName} onChange={handleChange} />
-                    {errors.projectName && <p className="text-red-500 text-sm">{errors.projectName}</p>}
+                    {errors.projectName && <p className="text-red-500 text-sm font-MarvelSans">{errors.projectName}</p>}
                 </div>
                 
                 <div className="mb-6">
@@ -75,8 +102,9 @@ const BriefForm = () => {
                     <textarea
                         id="goals"
                         name="goals"
-                        placeholder="Опишите ключевые цели и конкретные задачи, кот. данная функциональность решает"
-                        className={`${inputClasses} h-48 resize-none overflow-y-auto`}
+                        placeholder="Опишите основные цели и конкретные задачи, которые решает данная функциональность"
+                        className={`${inputClasses} h-auto resize-none overflow-y-hidden`} // Убираем фиксированную высоту
+                        ref={textareaGoalsRef} // Привязываем реф к textarea
                         value={formData.goals}
                         onChange={handleChange}
                     ></textarea>
@@ -87,24 +115,42 @@ const BriefForm = () => {
                     <label htmlFor="audience" className={labelClasses}>
                         Целевая аудитория
                     </label>
-                    <input type="text" id="audience" name="audience" placeholder="Основные пользователи" className={inputClasses} value={formData.audience} onChange={handleChange} />
+                    <textarea
+                        id="audience"
+                        name="audience"
+                        placeholder="Определите основных пользователей"
+                        className={`${inputClasses} h-auto resize-none overflow-y-hidden`} // Убираем фиксированную высоту
+                        ref={textareaAudienceRef} // Привязываем реф к textarea целевой аудитории
+                        value={formData.audience}
+                        onChange={handleChange}
+                    ></textarea>
+
+                   {/*} <input type="text" id="audience" name="audience" placeholder="Определите основных пользователей" className={inputClasses} value={formData.audience} onChange={handleChange} />*/}
+              
                 </div>
 
                 <div className="mb-6">
                     <label htmlFor="fullname" className={labelClasses}>
                         Автор идеи
                     </label>
-                    <input type="text" id="fullname" name="fullname" placeholder="Укажите ФИО" className={inputClasses} value={formData.fullname} onChange={handleChange} />
-                    {errors.fullname && <p className="text-red-500 text-sm">{errors.fullname}</p>}
+                    <input type="text" id="fullname" name="fullname" placeholder="Укажите полное имя автора" className={inputClasses} value={formData.fullname} onChange={handleChange} />
+                    {errors.fullname && <p className="text-red-500 text-sm font-MarvelSans">{errors.fullname}</p>}
                 </div>
 
                 {/* Кнопка отправки */}
+
                 <button 
                     type="submit"
                     disabled={!formData.goals || !formData.fullname}
-                    className={`bg-blue-500 text-white hover:bg-blue-400 px-6 py-3 rounded-md w-full transition duration-200 shadow-md ${!formData.goals || !formData.fullname ? 'opacity-50 cursor-not-allowed' : 'opacity-100'}`}>
+                    className={`custom-button ${!formData.goals || !formData.fullname ? 'opacity-50' : ''}`}>
                     Отправить
                 </button>
+            {/*   <button 
+                    type="submit"
+                    disabled={!formData.goals || !formData.fullname}
+                    className={`bg-blue-500 text-white hover:bg-blue-400 px-6 py-3 rounded-md w-full transition duration-200 shadow-md  ${!formData.goals || !formData.fullname ? 'opacity-50 cursor-not-allowed' : 'opacity-100'}`}>
+                    Отправить
+                </button> */}
             </div>
         </form>
     );
