4884e928669300d2524f97738983d8ce2b846e41
olgaolsh <oliprogram@gmail.com>
2024-09-27 22:29:41 +0300

    25sizeh

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -17,24 +17,19 @@ const Home = () => {
             window.removeEventListener('touchmove', preventScroll);
         };
     }, []);
-
-   {/* useEffect(() => {
-        // Прокручиваем страницу вверх при открытии компонента
-        window.scrollTo({ top: 200, behavior: 'smooth' });
-    }, []); // Пустой массив зависимостей означает, что этот эффект выполнится только один раз при монтировании */}
     return (
-        <div className="fullscreen-container">
-        <div className="image-container">
-            <img src={imageUrl} alt="Идея" className="image" />
+        <div className="fullscreen-container_idea">
+            <div className="image-container_idea">
+                <img src={imageUrl} alt="Идея" className="image_idea" />
+            </div>
+            <div className="home-container">
+                <h1>Ваши идеи — это ключ к преобразованиям!</h1>
+                <p>Бриф о разработке нового функционала для MS Dynamics AX.</p>
+                <p>Не стесняйтесь делиться своими предложениями и мыслями — каждая идея для нас бесценна!</p>
+                <p>Ваше активное участие поможет нам создавать решения, которые повысят продуктивность и эффективность работы в MS Dynamics AX.</p>
+            </div>
+            <a href="/briefform" className="brief-button_idea">Составить Бриф</a> {/* Кнопка здесь */}
         </div>
-        <div className="home-container">
-            <h1>Ваши идеи — это ключ к преобразованиям!</h1>
-            <p>Бриф о разработке нового функционала для MS Dynamics AX.</p>
-            <p>Не стесняйтесь делиться своими предложениями и мыслями — каждая идея для нас бесценна!</p>
-            <p>Ваше активное участие поможет нам создавать решения, которые повысят продуктивность и эффективность работы в MS Dynamics AX.</p>
-            <a href="/briefform" className="brief-button">Составить Бриф</a>
-        </div>
-    </div>
     );
 };
 
