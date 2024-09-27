9fee82d91c2ea3bfb68d3ef13cf9fca931000ea4
olgaolsh <oliprogram@gmail.com>
2024-09-27 19:04:29 +0300

    27chan

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './Home.css';
-
+import imageUrl from './../../img/imgforLinkIdeas.jpg';
 
 const Home = () => {
     useEffect(() => {
@@ -8,12 +8,17 @@ const Home = () => {
         window.scrollTo({ top: 200, behavior: 'smooth' });
     }, []); // Пустой массив зависимостей означает, что этот эффект выполнится только один раз при монтировании
     return (
+        <div>
+        <div className="image-container">
+            <img src={imageUrl} alt="Идея" className="image" />
+        </div>
         <div className="home-container">
         <h1>Ваши идеи — это ключ к преобразованиям!</h1>
         <p>Бриф о разработке нового функционала для MS Dynamics AX.</p>
         <p>Не стесняйтесь делиться своими предложениями и мыслями — каждая идея для нас бесценна!</p>
         <p>Ваше активное участие поможет нам создавать решения, которые повысят продуктивность и эффективность работы в MS Dynamics AX.</p>
             <a href="/briefform" className="brief-button">Составить Бриф</a>
+        </div>
         </div>
     );
 };
