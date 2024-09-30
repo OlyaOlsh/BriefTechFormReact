09b192f2c6cf00f6f3ac4346d9737104aa447b7a
olgaolsh <oliprogram@gmail.com>
2024-09-30 22:08:15 +0300

    51с

--- src/page/BriefFormNew/BriefFormNew.jsx
+++ src/page/BriefFormNew/BriefFormNew.jsx
@@ -7,7 +7,7 @@ import './BriefFormNew.css';
 
 
 const inputClasses = "w-full px-3 py-2 placeholder-input text-input bg-blue-100 rounded-lg mb-4 font-MarvelSans-Regular";
-const buttonClasses = 'bg-gradient-to-r from-blue-500 to-blue-700 text-white w-full py-3 px-6 rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-xl font-MarvelSans-Regular active:scale-95 active:shadow-inner'; // Добавлен эффект нажатия
+const buttonClasses = 'bg-gradient-to-r from-blue-500 to-blue-700 text-white w-full py-3 px-6 rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-xl font-MarvelSans-Regular active:scale-95 active:shadow-inner';
 
 const BriefFormNew = () => {
     const tg = window.Telegram?.WebApp;
@@ -22,6 +22,7 @@ const BriefFormNew = () => {
     const textareaAudienceRef = useRef(null);
     const textareaGoalsRef = useRef(null);
     const [buttonText, setButtonText] = useState('Отправить');
+    const [isSubmitting, setIsSubmitting] = useState(false); // Состояние для отслеживания нажатия кнопки
     const submitButtonRef = useRef(null); // Реф для кнопки
 
     useEffect(() => {
@@ -107,7 +108,10 @@ const BriefFormNew = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if (!validateForm()) return;
+
+        if (!validateForm() || isSubmitting) return; // Проверяем валидность формы и состояние кнопки
+
+        setIsSubmitting(true); // Устанавливаем состояние на "нажата"
 
         try {
             await addDoc(collection(db, 'IdeaTable'), formData);
@@ -126,22 +130,15 @@ const BriefFormNew = () => {
 
             setTimeout(() => {
                 setButtonText('Отправить еще');
+                setIsSubmitting(false); // Возвращаем состояние кнопки обратно
             }, 3000);
 
         } catch (error) {
             console.error('Ошибка при сохранении данных:', error);
             alert('Произошла ошибка при сохранении данных.');
+            setIsSubmitting(false); // Возвращаем состояние кнопки обратно в случае ошибки
         }
     };
-
-  {/* const handleKeyDown = (e, nextRef) => {
-        if (e.key === 'Enter') {
-            e.preventDefault(); // Предотвращаем стандартное поведение Enter
-            if (nextRef && nextRef.current) {
-                nextRef.current.focus(); // Переводим фокус на следующее поле
-            }
-        }
-    };*/}
 
     const handleKeyDown = (e, nextRef) => {
         if (e.key === 'Enter') {
@@ -155,13 +152,13 @@ const BriefFormNew = () => {
             }
         }
     };
+    
     const handleFocus = () => {
         window.scrollTo({
             top: document.body.scrollHeight,
             behavior: 'smooth'
         });
     };
-
 
     const audienceRef = useRef(null);
     const fullnameRef = useRef(null);
@@ -170,68 +167,68 @@ const BriefFormNew = () => {
     const userName = tg?.initDataUnsafe?.user?.userName || "Гость";
 
     return (
-        <div className="bg-background text-primary-foreground p-4 rounded-lg shadow-lg flex flex-col h-screen justify-between">
-        <div className="flex-grow">
-            <h2 className="text-lg font-bold mb-4">Идея</h2>
-            <input 
-                type="text" 
-                name="projectName" 
-                value={formData.projectName} 
-                onChange={handleChange} 
-                onKeyDown={(e) => handleKeyDown(e, textareaGoalsRef)} // Переход на текстовое поле
-                placeholder="Укажите краткое название" 
-                className={inputClasses} 
-            />
-            <h2 className="text-lg font-bold mb-4">Цели и задачи</h2> 
-            <textarea 
-                name="goals" 
-                ref={textareaGoalsRef} 
-                value={formData.goals} 
-                onChange={handleChange} 
-                onKeyDown={(e) => handleKeyDown(e, audienceRef)} // Переход на следующее поле
-                placeholder="Опишите кратко основные цели и задачи" 
-                className={inputClasses}
-            ></textarea>
-            <h2 className="text-lg font-bold mb-4">Для кого</h2>
-            <input 
-                type="text" 
-                name="audience" 
-                ref={audienceRef}
-                value={formData.audience}  
-                onChange={handleChange} 
-                onFocus={handleFocus} 
-                onKeyDown={(e) => handleKeyDown(e, fullnameRef)} // Переход на следующее поле
-                placeholder="Укажите для какого отдела" 
-                className={inputClasses} 
-            />
-            <h2 className="text-lg font-bold mb-4">Автор идеи</h2>
-            <input 
-                type="text" 
-                name="fullname" 
-                ref={fullnameRef}
-                value={formData.fullname} 
-                onChange={handleChange} 
-                onKeyDown={(e) => handleKeyDown(e, submitButtonRef)} // Переход к кнопке
-                placeholder="Укажите полное имя автора" 
-                className={inputClasses} 
-            />
-            {errors.projectName && <p className="text-red-500">{errors.projectName}</p>}
-            {errors.goals && <p className="text-red-500">{errors.goals}</p>}
-            {errors.fullname && <p className="text-red-500">{errors.fullname}</p>}
-        </div>
+        <form onSubmit={handleSubmit} className="bg-background">
+            <div className="bg-background text-primary-foreground p-4 rounded-lg shadow-lg flex flex-col h-screen justify-between">
+                <div className="flex-grow">
+                    <h2 className="text-lg font-bold mb-4">Идея</h2>
+                    <input 
+                        type="text" 
+                        name="projectName" 
+                        value={formData.projectName} 
+                        onChange={handleChange} 
+                        onKeyDown={(e) => handleKeyDown(e, textareaGoalsRef)} // Переход на текстовое поле
+                        placeholder="Укажите краткое название" 
+                        className={inputClasses} 
+                    />
+                    <h2 className="text-lg font-bold mb-4">Цели и задачи</h2> 
+                    <textarea 
+                        name="goals" 
+                        ref={textareaGoalsRef} 
+                        value={formData.goals} 
+                        onChange={handleChange} 
+                        onKeyDown={(e) => handleKeyDown(e, audienceRef)} // Переход на следующее поле
+                        placeholder="Опишите кратко основные цели и задачи" 
+                        className={inputClasses}
+                    ></textarea>
+                    <h2 className="text-lg font-bold mb-4">Для кого</h2>
+                    <input 
+                        type="text" 
+                        name="audience" 
+                        ref={audienceRef}
+                        value={formData.audience}  
+                        onChange={handleChange} 
+                        onFocus={handleFocus} 
+                        onKeyDown={(e) => handleKeyDown(e, fullnameRef)} // Переход на следующее поле
+                        placeholder="Укажите для какого отдела" 
+                        className={inputClasses} 
+                    />
+                    <h2 className="text-lg font-bold mb-4">Автор идеи</h2>
+                    <input 
+                        type="text" 
+                        name="fullname" 
+                        ref={fullnameRef}
+                        value={formData.fullname} 
+                        onChange={handleChange} 
+                        onKeyDown={(e) => handleKeyDown(e, submitButtonRef)} // Переход к кнопке
+                        placeholder="Укажите полное имя автора" 
+                        className={inputClasses} 
+                    />
+                    {errors.projectName && <p className="text-red-500">{errors.projectName}</p>}
+                    {errors.goals && <p className="text-red-500">{errors.goals}</p>}
+                    {errors.fullname && <p className="text-red-500">{errors.fullname}</p>}
+                </div>
 
-         {/* Фиксированная кнопка */}
-         <div className="bg-background">
-                <button ref={submitButtonRef} className={buttonClasses} onClick={handleSubmit}>
-                    Отправить
+                <button 
+                    ref={submitButtonRef} 
+                    className={`${buttonClasses} ${isSubmitting ? 'bg-[#78C946]' : ''}`} // Изменяем цвет при нажатии
+                    type="submit" 
+                    disabled={isSubmitting} // Отключаем кнопку во время обработки
+                >
+                    {buttonText}
                 </button>
-         </div>
-        {/* Фиксированная кнопка */}
-       {/* <div className="fixed bottom-0 left-0 right-0 p-4 bg-background">
-            <button className={buttonClasses} onClick={handleSubmit}>{buttonText}</button>
-        </div>*/}
-    </div>
-);
+            </div>
+        </form>
+    );
 };
 
 export default BriefFormNew;
